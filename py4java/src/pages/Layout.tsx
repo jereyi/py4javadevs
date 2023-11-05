2ea697b3202960c9becdd2eeb536d9880eed0fb0
jereyi <jereyi@princeton.edu>
2023-11-05 21:05:01 +0000

    Use navigate instead of href for logo

--- py4java/src/pages/Layout.tsx
+++ py4java/src/pages/Layout.tsx
@@ -36,11 +36,9 @@ const Layout = () => {
       {user && location.pathname !== "/login" ? (
         <nav className="bg-dim-gray border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl py-4 px-16">
-            <a href="/" className="flex items-center">
-              <span className="self-center text-2xl font-cal font-semibold whitespace-nowrap text-white">
+              <span className="self-center text-2xl font-cal font-semibold whitespace-nowrap text-white cursor-pointer" onClick={() => navigate("/")}>
                 Python for Java Devs
               </span>
-            </a>
           </div>
         </nav>
       ) : (
